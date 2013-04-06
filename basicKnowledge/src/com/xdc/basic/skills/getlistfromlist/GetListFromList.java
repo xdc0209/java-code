73c9663d0f19a6edc720f50971d6b37b9b09d0f5
XuDachao <xdc0209@qq.com>
2013-04-07 00:40:59 +0800

    google from list to list

--- basicKnowledge/src/com/xdc/basic/skills/getlistfromlist/GetListFromList.java
+++ basicKnowledge/src/com/xdc/basic/skills/getlistfromlist/GetListFromList.java
@@ -3,6 +3,9 @@ package com.xdc.basic.skills.getlistfromlist;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.google.common.base.Function;
+import com.google.common.collect.Lists;
+
 public class GetListFromList
 {
 
@@ -15,7 +18,20 @@ public class GetListFromList
 		students.add(new Student("20082890", "xudachao"));
 		students.add(new Student("20082889", "wangziwei"));
 
-		// TODO: 待实现
-		List<String> numbers;
+		// Google 提供的类库
+		List<String> numbers = Lists.transform(students, new Function<Student, String>()
+		{
+			public String apply(Student input)
+			{
+				return input.getNumber();
+			}
+		});
+
+		System.out.println("仅输出学号：");
+		for (String number : numbers)
+		{
+			System.out.println(number);
+		}
+
 	}
 }
