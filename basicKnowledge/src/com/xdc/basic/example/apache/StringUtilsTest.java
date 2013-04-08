e437a472eb6bb94d0f584fad93f7514a79d4bf9c
XuDachao <xdc0209@qq.com>
2013-04-09 00:43:29 +0800

    StringUtils

---
+++ basicKnowledge/src/com/xdc/basic/example/apache/StringUtilsTest.java
@@ -0,0 +1,35 @@
+package com.xdc.basic.example.apache;
+
+import java.util.Locale;
+
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * 字符串常用操作
+ * 
+ * @author xdc
+ * 
+ */
+public class StringUtilsTest
+{
+	public static void main(String[] args)
+	{
+		// 判断某字符串是否为空，为空的标准是 str==null 或 str.length()==0
+		StringUtils.isEmpty(null);
+		StringUtils.isEmpty("");
+
+		// 判断某字符串是否为空或长度为0或由空白符(whitespace) 构成
+		StringUtils.isBlank(null);
+		StringUtils.isBlank("");
+		StringUtils.isBlank(" ");
+		StringUtils.isBlank("\t \n \f \r"); // 对于制表符、换行符、换页符和回车符
+
+		// 字符串变成小写，要设置区域，防止不同语言转换结果不一样，是程序有不一样的行为
+		String name = "XuDachao";
+		String lowerCaseName = StringUtils.lowerCase(name, Locale.US);
+
+		// 比较两字符串是相等
+		StringUtils.equals(lowerCaseName, name); // false
+		StringUtils.equals(lowerCaseName, "xudachao"); // true
+	}
+}
---
+++ basicKnowledge/src/com/xdc/basic/example/apache/Student.java
@@ -0,0 +1,52 @@
+package com.xdc.basic.example.apache;
+
+public class Student
+{
+	private String	num;
+
+	private String	name;
+
+	private String	address;
+
+	private String	phoneNum;
+
+	public String getNum()
+	{
+		return num;
+	}
+
+	public void setNum(String num)
+	{
+		this.num = num;
+	}
+
+	public String getName()
+	{
+		return name;
+	}
+
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+
+	public String getAddress()
+	{
+		return address;
+	}
+
+	public void setAddress(String address)
+	{
+		this.address = address;
+	}
+
+	public String getPhoneNum()
+	{
+		return phoneNum;
+	}
+
+	public void setPhoneNum(String phoneNum)
+	{
+		this.phoneNum = phoneNum;
+	}
+}
