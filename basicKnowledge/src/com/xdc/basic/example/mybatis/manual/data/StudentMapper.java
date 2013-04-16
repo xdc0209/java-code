63669ca5db80dc5a5d163025180be7c494975768
XuDachao <xdc0209@qq.com>
2013-04-16 22:21:07 +0800

    mybatis manaual

--- basicKnowledge/src/com/xdc/basic/example/mybatis/manual/data/StudentMapper.java
+++ basicKnowledge/src/com/xdc/basic/example/mybatis/manual/data/StudentMapper.java
@@ -1,33 +1,14 @@
 package com.xdc.basic.example.mybatis.manual.data;
 
-import java.util.List;
-
-import org.apache.ibatis.annotations.Param;
-
 import com.xdc.basic.example.mybatis.manual.model.Student;
-import com.xdc.basic.example.mybatis.manual.model.StudentExample;
 
 public interface StudentMapper
 {
-	int countByExample(StudentExample example);
-
-	int deleteByExample(StudentExample example);
+	int insert(Student record);
 
 	int deleteByPrimaryKey(Integer id);
 
-	int insert(Student record);
-
-	int insertSelective(Student record);
-
-	List<Student> selectByExample(StudentExample example);
-
 	Student selectByPrimaryKey(Integer id);
-
-	int updateByExampleSelective(@Param("record") Student record, @Param("example") StudentExample example);
-
-	int updateByExample(@Param("record") Student record, @Param("example") StudentExample example);
-
-	int updateByPrimaryKeySelective(Student record);
 
 	int updateByPrimaryKey(Student record);
 }
