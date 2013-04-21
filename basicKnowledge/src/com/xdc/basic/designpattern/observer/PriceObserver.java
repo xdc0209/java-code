46dfc5f8e837a30cf71dbe0cab8375abf573f0e5
XuDachao <xdc0209@qq.com>
2013-04-21 22:51:10 +0800

    观察�

---
+++ basicKnowledge/src/com/xdc/basic/designpattern/observer/PriceObserver.java
@@ -0,0 +1,20 @@
+package com.xdc.basic.designpattern.observer;
+
+import java.util.Observable;
+import java.util.Observer;
+
+// 观察者PriceObserver主要用来对产品价格(price)进行观察的
+public class PriceObserver implements Observer
+{
+    private float price = 0;
+
+    public void update(Observable obj, Object arg)
+    {
+        if (arg instanceof Float)
+        {
+            price = ((Float) arg).floatValue();
+            // 产品价格改变值在price中
+            System.out.println("PriceObserver: price changet to " + price);
+        }
+    }
+}
--- basicKnowledge/src/com/xdc/basic/example/RegexTest.java
+++ basicKnowledge/src/com/xdc/basic/example/RegexTest.java
@@ -7,46 +7,46 @@ import java.util.regex.Pattern;
 
 public class RegexTest
 {
-	public static void main(String[] args)
-	{
-		// 整数1 加或乘 整数2 用正则表达式进行输入验证
+    public static void main(String[] args)
+    {
+        // 整数1 加或乘 整数2 用正则表达式进行输入验证
 
-		@SuppressWarnings("resource")
-		Scanner cin = new Scanner(System.in);
+        @SuppressWarnings("resource")
+        Scanner cin = new Scanner(System.in);
 
-		String regex = "^\\s*(-?\\d+)\\s*([+*])\\s*(-?\\d+)\\s*$";
-		Pattern pattern = Pattern.compile(regex);
+        String regex = "^\\s*(-?\\d+)\\s*([+*])\\s*(-?\\d+)\\s*$";
+        Pattern pattern = Pattern.compile(regex);
 
-		while (true)
-		{
-			String line = cin.nextLine();
-			if (line.equals(""))
-			{
-				continue;
-			}
+        while (true)
+        {
+            String line = cin.nextLine();
+            if (line.equals(""))
+            {
+                continue;
+            }
 
-			Matcher matcher = pattern.matcher(line);
-			if (matcher.find())
-			{
-				ArrayList<String> itemsArrayListList = new ArrayList<String>();
-				// matcher.groupCount()方法返回 int 类型值，表示当前 Matcher 模式中捕获组的数量。本例中值为3。
-				// 有一个特别的组——组 0，它表示整个表达式。这个组不包括在 groupCount 的报告范围内。
-				// 因此下面的循环中，先加入整表达式，在依次放入：整数1， 加或乘， 整数2。
-				for (int i = 0; i <= matcher.groupCount(); i++)
-				{
-					String item = matcher.group(i);
-					itemsArrayListList.add(item);
-				}
-				for (String s : itemsArrayListList)
-				{
-					System.out.println(s);
-				}
-			}
-			else
-			{
-				System.out.println("表达式不合法!");
-				continue;
-			}
-		}
-	}
+            Matcher matcher = pattern.matcher(line);
+            if (matcher.find())
+            {
+                ArrayList<String> itemsArrayListList = new ArrayList<String>();
+                // matcher.groupCount()方法返回 int 类型值，表示当前 Matcher 模式中捕获组的数量。本例中值为3。
+                // 有一个特别的组——组 0，它表示整个表达式。这个组不包括在 groupCount 的报告范围内。
+                // 因此下面的循环中，先加入整表达式，在依次放入：整数1， 加或乘， 整数2。
+                for (int i = 0; i <= matcher.groupCount(); i++)
+                {
+                    String item = matcher.group(i);
+                    itemsArrayListList.add(item);
+                }
+                for (String s : itemsArrayListList)
+                {
+                    System.out.println(s);
+                }
+            }
+            else
+            {
+                System.out.println("表达式不合法!");
+                continue;
+            }
+        }
+    }
 }
