baf4773cb31582338bf14334cbd2af36d435321c
XuDachao <xdc0209@qq.com>
2013-06-18 01:06:17 +0800

    hibernate-validator 入门   注解入门

---
+++ basicKnowledge/src/com/xdc/basic/example/annotation/TestAnnotation.java
@@ -0,0 +1,47 @@
+package com.xdc.basic.example.annotation;
+
+import java.lang.reflect.Method;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * http://blog.csdn.net/sooneasy/article/details/4384888
+ * 
+ * @author xdc
+ * 
+ */
+public class TestAnnotation
+{
+    /**
+     * author lighter
+     * 说明:具体关天Annotation的API的用法请参见javaDoc文档
+     */
+    public static void main(String[] args) throws Exception
+    {
+        String CLASS_NAME = "com.xdc.basic.example.annotation.JavaEyer";
+        Class<?> test = Class.forName(CLASS_NAME);
+        Method[] method = test.getMethods();
+        boolean flag = test.isAnnotationPresent(Description.class);
+        if (flag)
+        {
+            Description des = (Description) test.getAnnotation(Description.class);
+            System.out.println("描述:" + des.value());
+            System.out.println("-----------------");
+        }
+
+        //把JavaEyer这一类有利用到@Name的全部方法保存到Set中去
+        Set<Method> set = new HashSet<Method>();
+        for (int i = 0; i < method.length; i++)
+        {
+            boolean otherFlag = method[i].isAnnotationPresent(Name.class);
+            if (otherFlag)
+                set.add(method[i]);
+        }
+        for (Method m : set)
+        {
+            Name name = m.getAnnotation(Name.class);
+            System.out.println(name.originate());
+            System.out.println("创建的社区:" + name.community());
+        }
+    }
+}
--- basicKnowledge/src/com/xdc/basic/java4android/io/IOCharStreamTest.java
+++ basicKnowledge/src/com/xdc/basic/java4android/io/IOCharStreamTest.java
@@ -11,53 +11,44 @@ import java.io.FileWriter;
  */
 class IOCharStreamTest
 {
-	public static void main(String[] args)
-	{
-		// 获取当前路径
-		String curClassName = new Throwable().getStackTrace()[0].getClassName();
-		String curPackage = curClassName.substring(0, curClassName.lastIndexOf("."));
-		String curPath = "src\\" + curPackage.replace(".", "\\") + "\\";
+    public static void main(String[] args)
+    {
+        // 获取当前路径
+        String curClassName = new Throwable().getStackTrace()[0].getClassName();
+        String curPackage = curClassName.substring(0, curClassName.lastIndexOf("."));
+        String curPath = "src\\" + curPackage.replace(".", "\\") + "\\";
 
-		String clazzName = new SecurityManager()
-		{
-			public String getClassName()
-			{
-				return getClassContext()[1].getName();
-			}
-		}.getClassName();
-		System.out.println(clazzName);
-
-		FileReader fr = null;
-		FileWriter fw = null;
-		try
-		{
-			fr = new FileReader(curPath + "from.txt");
-			fw = new FileWriter(curPath + "to.txt");
-			char[] buffer = new char[1024];
-			int len;
-			while ((len = fr.read(buffer, 0, buffer.length)) != -1)
-			{
-				fw.write(buffer, 0, len);
-				String s = new String(buffer);
-				s = s.trim();
-				System.out.println(s);
-			}
-		}
-		catch (Exception e)
-		{
-			System.out.println(e);
-		}
-		finally
-		{
-			try
-			{
-				fr.close();
-				fw.close();
-			}
-			catch (Exception e)
-			{
-				System.out.println(e);
-			}
-		}
-	}
+        FileReader fr = null;
+        FileWriter fw = null;
+        try
+        {
+            fr = new FileReader(curPath + "from.txt");
+            fw = new FileWriter(curPath + "to.txt");
+            char[] buffer = new char[1024];
+            int len;
+            while ((len = fr.read(buffer, 0, buffer.length)) != -1)
+            {
+                fw.write(buffer, 0, len);
+                String s = new String(buffer);
+                s = s.trim();
+                System.out.println(s);
+            }
+        }
+        catch (Exception e)
+        {
+            System.out.println(e);
+        }
+        finally
+        {
+            try
+            {
+                fr.close();
+                fw.close();
+            }
+            catch (Exception e)
+            {
+                System.out.println(e);
+            }
+        }
+    }
 }
