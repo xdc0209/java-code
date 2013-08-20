21b21341f7486ea24c62698a0c36d95137a96591
XuDachao <xdc0209@qq.com>
2013-08-21 00:42:21 +0800

    UTC

--- basicKnowledge/src/com/xdc/basic/apidemo/apache/commons/lang3/time/DateFormatUtilsTest.java
+++ basicKnowledge/src/com/xdc/basic/apidemo/apache/commons/lang3/time/DateFormatUtilsTest.java
@@ -6,7 +6,10 @@ public class DateFormatUtilsTest
 {
 	public static void main(String[] args)
 	{
-		System.out.println("格式化日期输出:");
+		System.out.println("本地格式日期输出:");
 		System.out.println(DateFormatUtils.format(System.currentTimeMillis(), "yyyy-MM-dd HH:mm:ss"));
+
+		System.out.println("UTC格式日期输出:");
+		System.out.println(DateFormatUtils.formatUTC(System.currentTimeMillis(), "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"));
 	}
 }
