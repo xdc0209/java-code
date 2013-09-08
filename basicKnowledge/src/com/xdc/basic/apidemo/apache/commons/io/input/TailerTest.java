85c87b5c945b17b1a32172c0659eca81de1f86ea
XuDachao <xdc0209@qq.com>
2013-09-08 13:47:33 +0800

    Tailer： linux下 tail -f

--- basicKnowledge/src/com/xdc/basic/apidemo/apache/commons/io/input/TailerTest.java
+++ basicKnowledge/src/com/xdc/basic/apidemo/apache/commons/io/input/TailerTest.java
@@ -1,12 +1,31 @@
 package com.xdc.basic.apidemo.apache.commons.io.input;
 
+import java.io.File;
+
+import org.apache.commons.io.input.Tailer;
+
 public class TailerTest
 {
-
 	public static void main(String[] args)
 	{
-		// TODO 自动生成的方法存根
+		// 获取当前路径
+		String curClassName = new Throwable().getStackTrace()[0].getClassName();
+		String curPackage = curClassName.substring(0, curClassName.lastIndexOf("."));
+		String curPath = "src\\" + curPackage.replace(".", "\\") + "\\";
 
+		Tailer tailer = Tailer.create(new File(curPath + "from.txt"), new MyTailerListener());
+
+		try
+		{
+			Thread.sleep(100000L);
+		}
+		catch (InterruptedException e)
+		{
+			e.printStackTrace();
+		}
+		finally
+		{
+			tailer.stop();
+		}
 	}
-
 }
