d48fe6b2482a953b6389a3ded716030eaa006b95
XuDachao <xdc0209@qq.com>
2013-09-27 23:58:17 +0800

    sha256

--- basicKnowledge/src/com/xdc/basic/apidemo/apache/commons/codec/digest/DigestUtilsTest.java
+++ basicKnowledge/src/com/xdc/basic/apidemo/apache/commons/codec/digest/DigestUtilsTest.java
@@ -19,17 +19,24 @@ public class DigestUtilsTest
 
 		InputStream inputMd5 = null;
 		InputStream inputSha1 = null;
+		InputStream inputSha256 = null;
 
 		String md5Hex;
 		try
 		{
+			// 算法强度小于sha1
 			inputMd5 = new FileInputStream(file);
 			md5Hex = DigestUtils.md5Hex(inputMd5);
 			System.out.println("MD5值:" + md5Hex);
 
+			// 算法强度小于sha256
 			inputSha1 = new FileInputStream(file);
 			String sha1Hex = DigestUtils.sha1Hex(inputSha1);
 			System.out.println("SHA1值:" + sha1Hex);
+
+			inputSha256 = new FileInputStream(file);
+			String sha256Hex = DigestUtils.sha256Hex(inputSha256);
+			System.out.println("SHA256值:" + sha256Hex);
 		}
 		catch (IOException e)
 		{
