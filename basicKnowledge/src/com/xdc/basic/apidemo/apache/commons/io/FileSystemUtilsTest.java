747317e11698cd0c81a72c3d3e90af1f81a35e09
XuDachao <xdc0209@qq.com>
2013-09-29 00:31:42 +0800

    查看路径所在分区剩余大小 FileSystemUtils.freeSpaceKb

---
+++ basicKnowledge/src/com/xdc/basic/apidemo/apache/commons/io/FileSystemUtilsTest.java
@@ -0,0 +1,25 @@
+package com.xdc.basic.apidemo.apache.commons.io;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileSystemUtils;
+import org.apache.commons.io.FileUtils;
+
+public class FileSystemUtilsTest
+{
+	public static void main(String[] args)
+	{
+		try
+		{
+			String path = new File(".").getAbsolutePath();
+			long freeSpaceKb = FileSystemUtils.freeSpaceKb(path, 5000L);
+			System.out.println(freeSpaceKb + "KB");
+			System.out.println(FileUtils.byteCountToDisplaySize(freeSpaceKb * FileUtils.ONE_KB));
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
+}
