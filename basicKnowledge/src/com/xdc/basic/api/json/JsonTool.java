b4547a76c97b55be618a958c0b5b4005c9a891b0
XuDachao <xdc0209@qq.com>
2013-11-04 00:11:59 +0800

    json smart

---
+++ basicKnowledge/src/com/xdc/basic/api/json/JsonTool.java
@@ -0,0 +1,102 @@
+package com.xdc.basic.api.json;
+
+import net.minidev.json.JSONArray;
+import net.minidev.json.JSONObject;
+import net.minidev.json.JSONStyle;
+import net.minidev.json.JSONValue;
+import net.minidev.json.parser.JSONParser;
+import net.minidev.json.parser.ParseException;
+
+import org.junit.Test;
+
+/*
+ * Home page: http://code.google.com/p/json-smart/
+ * 
+ */
+
+public class JsonTool
+{
+    @Test
+    public void EncodeJsonObject()
+    {
+        // Json Object is an HashMap<String, Object> extends
+        JSONObject obj = new JSONObject();
+        obj.put("name", "foo");
+        obj.put("num", 100);
+        obj.put("balance", 1000.21);
+        obj.put("is_vip", true);
+        obj.put("nickname", null);
+
+        System.out.println("Standard RFC4627 JSON");
+        System.out.println(obj.toJSONString());
+        System.out.println("Compacted JSON Value");
+        System.out.println(obj.toJSONString(JSONStyle.MAX_COMPRESS));
+
+        // if obj is an common map you can use:
+        System.out.println("Standard RFC4627 JSON");
+        System.out.println(JSONValue.toJSONString(obj));
+        System.out.println("Compacted JSON Value");
+        System.out.println(JSONValue.toJSONString(obj, JSONStyle.MAX_COMPRESS));
+    }
+
+    @Test
+    public void DecodingJsonText() throws ParseException
+    {
+        System.out.println("=======decode=======");
+
+        String s = "[0,{'1':{'2':{'3':{'4':[5,{'6':7}]}}}}]";
+        Object obj = JSONValue.parse(s);
+        JSONArray array = (JSONArray) obj;
+        System.out.println("======the 2nd element of array======");
+        System.out.println(array.get(1));
+        System.out.println();
+
+        JSONObject obj2 = (JSONObject) array.get(1);
+        System.out.println("======field \"1\"==========");
+        System.out.println(obj2.get("1"));
+
+        s = "{}";
+        obj = JSONValue.parse(s);
+        System.out.println(obj);
+
+        s = "{\"key\":\"Value\"}";
+        // JSONValue.parseStrict()
+        // can be use to be sure that the input is wellformed
+        obj = JSONValue.parseStrict(s);
+        JSONObject obj3 = (JSONObject) obj;
+        System.out.println("====== Object content ======");
+        System.out.println(obj3.get("key"));
+        System.out.println();
+    }
+
+    @Test
+    public void Merge2JsonObject() throws ParseException
+    {
+        String json1 = "{'car':{'color':'blue'}}";
+        String json2 = "{'car':{'size':'3.5m'}}";
+
+        JSONParser p = new JSONParser();
+        JSONObject o1 = (JSONObject) p.parse(json1);
+        JSONObject o2 = (JSONObject) p.parse(json2);
+
+        o1.merge(o2);
+
+        System.out.println(o1);
+    }
+
+    @Test
+    public void ValidatingJsonInput() throws ParseException
+    {
+        // JSONValue.isValidJson(String) To validate a string that conforms to the (non-strict) JSON Smart mode.
+        // JSONValue.isValidStrictJson(String) To validate a string of JSON for strict conformance to RFC4627.
+
+        String s = "{intValue:123}";
+        if (JSONValue.isValidJson(s))
+            System.out.println(s + " validates as Smart JSON");
+
+        if (JSONValue.isValidJsonStrict(s))
+            System.out.println(s + " validates as strict JSON");
+        else
+            System.out.println(s + " does not validate as strict JSON");
+    }
+}
