282a8b8f5b205797f1a32739371cec28f94f571c
XuDachao <xdc0209@qq.com>
2013-11-09 01:58:54 +0800

    run方法的规范写法： log and throw

--- basicKnowledge/src/com/xdc/basic/api/java4android/thread/implrunable/RunnableImpl.java
+++ basicKnowledge/src/com/xdc/basic/api/java4android/thread/implrunable/RunnableImpl.java
@@ -2,19 +2,34 @@ package com.xdc.basic.api.java4android.thread.implrunable;
 
 class RunnableImpl implements Runnable
 {
-	int	i	= 100;
+    int i = 100;
 
-	public void run()
-	{
-		while (true)
-		{
-			System.out.println(Thread.currentThread().getName() + "-->" + i);
-			i--;
-			Thread.yield();
-			if (i < 0)
-			{
-				break;
-			}
-		}
-	}
+    public void run()
+    {
+        while (true)
+        {
+            System.out.println(Thread.currentThread().getName() + "-->" + i);
+            i--;
+            Thread.yield();
+            if (i < 0)
+            {
+                break;
+            }
+        }
+
+    }
+
+    // 这个应该是规范的写法，如果不这么写，出错很难发现
+    //    public void run()
+    //    {
+    //        try
+    //        {
+    //            // dosomething    
+    //        }
+    //        catch (Throwable e)
+    //        {
+    //            // log and throw    
+    //            throw e;
+    //        }
+    //    }
 }
