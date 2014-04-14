fd3b534648ddcaf92b0d45b0069dc30c8605174c
XuDachao <xdc0209@qq.com>
2014-04-14 23:03:10 +0800

    线程中断

---
+++ basicKnowledge/src/com/xdc/basic/api/thread/InterruptTest.java
@@ -0,0 +1,40 @@
+package com.xdc.basic.api.thread;
+
+public class InterruptTest
+{
+
+    public static void main(String[] args)
+    {
+        Thread t = new Thread()
+        {
+            public void run()
+            {
+                System.out.println("执行run方法");
+                try
+                {
+                    Thread.sleep(10000);
+                    System.out.println("线程完成休眠");
+                }
+                catch (InterruptedException e)
+                {
+                    System.out.println("休眠被打断");
+                    return; // 返回到程序的调用处
+                }
+                System.out.println("线程正常终止");
+            }
+        };
+
+        t.start();
+
+        try
+        {
+            Thread.sleep(2000L);
+        }
+        catch (InterruptedException e)
+        {
+            e.printStackTrace();
+        }
+
+        t.interrupt(); // 2s后中断线程
+    }
+}
