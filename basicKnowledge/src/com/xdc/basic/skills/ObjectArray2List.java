f7198c845e9b5caa36e4a78edf9f11af3ab17b42
XuDachao <xdc0209@qq.com>
2014-05-12 00:20:03 +0800

    生成list简便做法

--- basicKnowledge/src/com/xdc/basic/skills/ObjectArray2List.java
+++ basicKnowledge/src/com/xdc/basic/skills/ObjectArray2List.java
@@ -12,18 +12,19 @@ import java.util.List;
  */
 public class ObjectArray2List
 {
-	@SuppressWarnings("unused")
-	public static void main(String[] args)
-	{
-		// 对象数组-->列表
-		String[] strArray = new String[] { "z", "a", "C" };
-		List<String> list = Arrays.asList(strArray); // 列表不支持修改操作
-		List<String> list2 = new ArrayList<String>(Arrays.asList(strArray)); // 推荐,列表支持修改操作
+    @SuppressWarnings("unused")
+    public static void main(String[] args)
+    {
+        // 对象数组-->列表
+        String[] strArray = new String[] { "z", "a", "C" };
+        List<String> list = Arrays.asList(strArray); // 列表不支持修改操作
+        //也可以直接这样写 List<String> list = Arrays.asList("z", "a", "C");
+        List<String> list2 = new ArrayList<String>(Arrays.asList(strArray)); // 推荐,列表支持修改操作
 
-		// 列表-->对象数组
-		ArrayList<String> sl = new ArrayList<String>();
-		String s[] = sl.toArray(new String[sl.size()]);
-		System.out.println(sl.getClass().getCanonicalName());
-		System.out.println(s.getClass().getCanonicalName());
-	}
+        // 列表-->对象数组
+        ArrayList<String> sl = new ArrayList<String>();
+        String s[] = sl.toArray(new String[sl.size()]);
+        System.out.println(sl.getClass().getCanonicalName());
+        System.out.println(s.getClass().getCanonicalName());
+    }
 }
