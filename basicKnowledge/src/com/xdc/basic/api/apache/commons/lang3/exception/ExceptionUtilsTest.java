1c7534ebd7120ee692561c9fa663ef8a913391ca
XuDachao <xdc0209@qq.com>
2014-08-21 21:40:31 +0800

    ExceptionUtilsTest

--- basicKnowledge/src/com/xdc/basic/api/apache/commons/lang3/exception/ExceptionUtilsTest.java
+++ basicKnowledge/src/com/xdc/basic/api/apache/commons/lang3/exception/ExceptionUtilsTest.java
@@ -13,19 +13,35 @@ public class ExceptionUtilsTest
         }
         catch (Throwable e)
         {
+            // 原始方法
+            e.getMessage();
+            e.toString(); // 内部调用了 e.getMessage();
+            e.printStackTrace(); // 内部调用了 e.toString();
+
+            // 原始方法获得rootCause
+            Throwable rootCause = getRootCause(e);
+
+            // 下面为apache包装类，内部实现调用的原始方法
             String message = ExceptionUtils.getMessage(e);
             String stackTrace = ExceptionUtils.getStackTrace(e);
 
-            e.getMessage();
-            e.printStackTrace();
-            e.toString();
-            while (e.getCause() != null)
-            {
-                e = e.getCause();
-            }
-            e.getMessage();
-            e.printStackTrace();
-            e.toString();
+            String rootCauseMessage = ExceptionUtils.getRootCauseMessage(e);
+            String[] rootCauseStackTrace = ExceptionUtils.getRootCauseStackTrace(e);
+
+            // apache包装类获得rootCause
+            rootCause = ExceptionUtils.getRootCause(e);
         }
+    }
+
+    /**
+     * 原始方法获得rootCause
+     */
+    private static Throwable getRootCause(Throwable e)
+    {
+        while (e.getCause() != null)
+        {
+            e = e.getCause();
+        }
+        return e;
     }
 }
