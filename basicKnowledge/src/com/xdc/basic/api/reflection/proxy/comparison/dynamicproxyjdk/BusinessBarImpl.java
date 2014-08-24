8a331f720c9e2024ad1c421d2afa23a106349b71
XuDachao <xdc0209@qq.com>
2014-08-25 01:32:44 +0800

    添加override

--- basicKnowledge/src/com/xdc/basic/api/reflection/proxy/comparison/dynamicproxyjdk/BusinessBarImpl.java
+++ basicKnowledge/src/com/xdc/basic/api/reflection/proxy/comparison/dynamicproxyjdk/BusinessBarImpl.java
@@ -2,6 +2,7 @@ package com.xdc.basic.api.reflection.proxy.comparison.dynamicproxyjdk;
 
 public class BusinessBarImpl implements BusinessBar
 {
+    @Override
     public String bar(String message)
     {
         System.out.println("BusinessBarImpl.bar()");
--- basicKnowledge/src/com/xdc/basic/api/reflection/proxy/comparison/dynamicproxyjdk/BusinessFooImpl.java
+++ basicKnowledge/src/com/xdc/basic/api/reflection/proxy/comparison/dynamicproxyjdk/BusinessFooImpl.java
@@ -2,6 +2,7 @@ package com.xdc.basic.api.reflection.proxy.comparison.dynamicproxyjdk;
 
 public class BusinessFooImpl implements BusinessFoo
 {
+    @Override
     public void foo()
     {
         System.out.println("BusinessFooImpl.foo()");
