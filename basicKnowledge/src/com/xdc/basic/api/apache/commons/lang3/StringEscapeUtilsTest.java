48dd724e78c84cfe9e0fc2121bbd45a8f958f212
XuDachao <xdc0209@qq.com>
2014-12-18 01:10:15 +0800

    StringEscapeUtils 样例

---
+++ basicKnowledge/src/com/xdc/basic/api/apache/commons/lang3/StringEscapeUtilsTest.java
@@ -0,0 +1,43 @@
+package com.xdc.basic.api.apache.commons.lang3;
+
+import org.apache.commons.lang3.StringEscapeUtils;
+
+public class StringEscapeUtilsTest
+{
+    @SuppressWarnings("unused")
+    public static void main(String[] args)
+    {
+        String java = "He didn't say, \"Stop!\"";
+        String JavaScript = "He didn't say, \"Stop!\"";
+        String xml = "\"bread\" & \"butter\"";
+        String html4 = "\"bread\" & \"butter\"";
+
+        String escapeJava = StringEscapeUtils.escapeJava(java);
+        String escapeEcmaScript = StringEscapeUtils.escapeEcmaScript(JavaScript);  // EcmaScript，即JavaScript
+        String escapeXml = StringEscapeUtils.escapeXml(xml);
+        String escapeHtml4 = StringEscapeUtils.escapeHtml4(html4);
+
+        String unescapeJava = StringEscapeUtils.unescapeJava(escapeJava);
+        String unescapeEcmaScript = StringEscapeUtils.unescapeEcmaScript(escapeEcmaScript);
+        String unescapeXml = StringEscapeUtils.unescapeXml(escapeXml);
+        String unescapeHtml4 = StringEscapeUtils.unescapeHtml4(escapeHtml4);
+
+        // 其实上面的方法内部调用下面的方法进行实现的
+        //  StringEscapeUtils.ESCAPE_CSV.translate(input);
+        //  StringEscapeUtils.ESCAPE_ECMASCRIPT.translate(input);
+        //  StringEscapeUtils.ESCAPE_HTML3.translate(input);
+        //  StringEscapeUtils.ESCAPE_HTML4.translate(input);
+        //  StringEscapeUtils.ESCAPE_JAVA.translate(input);
+        //  StringEscapeUtils.ESCAPE_XML.translate(input);
+        //  
+        //  StringEscapeUtils.UNESCAPE_CSV.translate(input);
+        //  StringEscapeUtils.UNESCAPE_ECMASCRIPT.translate(input);
+        //  StringEscapeUtils.UNESCAPE_HTML3.translate(input);
+        //  StringEscapeUtils.UNESCAPE_HTML4.translate(input);
+        //  StringEscapeUtils.UNESCAPE_JAVA.translate(input);
+        //  StringEscapeUtils.UNESCAPE_XML.translate(input);
+
+        System.out.println();
+
+    }
+}
