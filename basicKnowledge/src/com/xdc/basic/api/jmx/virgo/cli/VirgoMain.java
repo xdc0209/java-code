e5af6bb03ce4f60ba1a763fab6d8e1929a5c55a2
XuDachao <xdc0209@qq.com>
2014-12-18 23:16:06 +0800

    jmx 连接virgo

--- basicKnowledge/src/com/xdc/basic/api/jmx/virgo/cli/VirgoMain.java
+++ basicKnowledge/src/com/xdc/basic/api/jmx/virgo/cli/VirgoMain.java
@@ -4,7 +4,7 @@ public class VirgoMain
 {
     public static void main(String[] args)
     {
-        args = new String[] { "bundle", "-n", "org.eclipse.virgo.kernel", "-v", "3.6.2", "-s", "active", "-t" };
+        // args = new String[] { "bundle", "-n", "org.eclipse.virgo.kernel", "-v", "3.6.2", "-s", "active", "-t" };
 
         try
         {
