94a8dec5e13ddfa3f4468b5d07668be97f82345c
XuDachao <xdc0209@qq.com>
2014-12-30 00:40:11 +0800

    阻塞队列

---
+++ basicKnowledge/src/com/xdc/basic/api/thread/executor/asyncprocess/AbstractProcess.java
@@ -0,0 +1,27 @@
+package com.xdc.basic.api.thread.executor.asyncprocess;
+
+public abstract class AbstractProcess
+{
+    public String asyncProcess(final String para)
+    {
+        ThreadExecutor.getInstance().asyncExec(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                try
+                {
+                    process(para);
+                }
+                catch (Exception e)
+                {
+                    e.printStackTrace();
+                }
+            }
+
+        });
+        return "success!";
+    }
+
+    abstract void process(String para);
+}
