eeaf0e28ff58e1090b7acee672228a15af42d11d
XuDachao <xdc0209@qq.com>
2015-01-03 12:50:38 +0800

    Pair

---
+++ basicKnowledge/src/com/xdc/basic/skills/pair/Pair.java
@@ -0,0 +1,102 @@
+package com.xdc.basic.skills.pair;
+
+import java.io.Serializable;
+
+public class Pair<T1, T2> implements Serializable
+{
+    private static final long serialVersionUID = 2598094911126860655L;
+
+    private T1                first;
+
+    private T2                second;
+
+    public Pair(T1 first, T2 second)
+    {
+        this.first = first;
+        this.second = second;
+    }
+
+    public T1 getFirst()
+    {
+        return first;
+    }
+
+    public void setFirst(T1 first)
+    {
+        this.first = first;
+    }
+
+    public T2 getSecond()
+    {
+        return second;
+    }
+
+    public void setSecond(T2 second)
+    {
+        this.second = second;
+    }
+
+    @Override
+    public String toString()
+    {
+        return first + ":" + second;
+    }
+
+    /**
+     * 通过eclipse生成
+     */
+    @Override
+    public int hashCode()
+    {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((first == null) ? 0 : first.hashCode());
+        result = prime * result + ((second == null) ? 0 : second.hashCode());
+        return result;
+    }
+
+    /**
+     * 通过eclipse生成
+     */
+    @Override
+    public boolean equals(Object obj)
+    {
+        if (this == obj)
+        {
+            return true;
+        }
+        if (obj == null)
+        {
+            return false;
+        }
+        if (getClass() != obj.getClass())
+        {
+            return false;
+        }
+        Pair<?, ?> other = (Pair<?, ?>) obj;
+        if (first == null)
+        {
+            if (other.first != null)
+            {
+                return false;
+            }
+        }
+        else if (!first.equals(other.first))
+        {
+            return false;
+        }
+        if (second == null)
+        {
+            if (other.second != null)
+            {
+                return false;
+            }
+        }
+        else if (!second.equals(other.second))
+        {
+            return false;
+        }
+        return true;
+    }
+
+}
---
+++ basicKnowledge/src/com/xdc/basic/skills/pair/StringPair.java
@@ -0,0 +1,100 @@
+package com.xdc.basic.skills.pair;
+
+import java.io.Serializable;
+
+public class StringPair implements Serializable
+{
+    private static final long serialVersionUID = 3118267258639752674L;
+
+    private String            first;
+    private String            second;
+
+    public StringPair(String first, String second)
+    {
+        this.first = first;
+        this.second = second;
+    }
+
+    public String getFirst()
+    {
+        return first;
+    }
+
+    public void setFirst(String first)
+    {
+        this.first = first;
+    }
+
+    public String getSecond()
+    {
+        return second;
+    }
+
+    public void setSecond(String second)
+    {
+        this.second = second;
+    }
+
+    @Override
+    public String toString()
+    {
+        return first + ":" + second;
+    }
+
+    /**
+     * 通过eclipse生成
+     */
+    @Override
+    public int hashCode()
+    {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((first == null) ? 0 : first.hashCode());
+        result = prime * result + ((second == null) ? 0 : second.hashCode());
+        return result;
+    }
+
+    /**
+     * 通过eclipse生成
+     */
+    @Override
+    public boolean equals(Object obj)
+    {
+        if (this == obj)
+        {
+            return true;
+        }
+        if (obj == null)
+        {
+            return false;
+        }
+        if (getClass() != obj.getClass())
+        {
+            return false;
+        }
+        StringPair other = (StringPair) obj;
+        if (first == null)
+        {
+            if (other.first != null)
+            {
+                return false;
+            }
+        }
+        else if (!first.equals(other.first))
+        {
+            return false;
+        }
+        if (second == null)
+        {
+            if (other.second != null)
+            {
+                return false;
+            }
+        }
+        else if (!second.equals(other.second))
+        {
+            return false;
+        }
+        return true;
+    }
+}
