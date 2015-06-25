9c3439509a558e4da9a19b77c928eaf95720701c
xdc0209 <xdc0209@qq.com>
2015-06-26 01:00:57 +0800

    mq

---
+++ basicKnowledge/src/com/xdc/basic/api/mq/activemq/xdc.java
@@ -0,0 +1,11 @@
+package com.xdc.basic.api.mq.activemq;
+
+public class xdc
+{
+    public static void main(String[] args)
+    {
+        //        ActiveMQTopic connectionAdvisoryTopic = AdvisorySupport.getConnectionAdvisoryTopic();
+        //        AdvisorySupport.getConsumerAdvisoryTopic(destination);
+        //        AdvisorySupport.getProducerAdvisoryTopic(destination);
+    }
+}
