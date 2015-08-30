9fd0192f04d21e8b5f12c79b1276ea756737701b
xdc0209 <xdc0209@qq.com>
2015-08-31 00:56:15 +0800

    jvm信息获取

---
+++ basicKnowledge/src/com/xdc/basic/api/jvm/Deadlock.java
@@ -0,0 +1,48 @@
+package com.xdc.basic.api.jvm;
+
+import java.lang.management.ManagementFactory;
+import java.lang.management.ThreadInfo;
+import java.lang.management.ThreadMXBean;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.lang3.ArrayUtils;
+import org.apache.commons.lang3.SystemUtils;
+
+public class Deadlock
+{
+    public static void main(String[] args)
+    {
+        ThreadMXBean threadMXBean = ManagementFactory.getThreadMXBean();
+
+        List<ThreadInfo> deadlockedThreadInfos = new ArrayList<ThreadInfo>();
+
+        long[] deadlockedThreadIds = threadMXBean.findDeadlockedThreads();
+        if (ArrayUtils.isNotEmpty(deadlockedThreadIds))
+        {
+            ThreadInfo[] allThreadInfos = threadMXBean.dumpAllThreads(true, true);
+            for (long deadlockedThreadId : deadlockedThreadIds)
+            {
+                for (ThreadInfo threadInfo : allThreadInfos)
+                {
+                    if (threadInfo.getThreadId() == deadlockedThreadId)
+                    {
+                        deadlockedThreadInfos.add(threadInfo);
+                    }
+                }
+            }
+
+            StringBuilder sb = new StringBuilder();
+            sb.append("Found java level deadlocks, java stack information for the deadlock threads:");
+            sb.append(SystemUtils.LINE_SEPARATOR);
+            sb.append("============================================================================");
+            sb.append(SystemUtils.LINE_SEPARATOR);
+
+            for (ThreadInfo threadInfo : deadlockedThreadInfos)
+            {
+                sb.append(threadInfo);
+            }
+            System.out.println(sb);
+        }
+    }
+}
---
+++ basicKnowledge/src/com/xdc/basic/api/jvm/Heap.java
@@ -0,0 +1,55 @@
+package com.xdc.basic.api.jvm;
+
+import java.lang.management.GarbageCollectorMXBean;
+import java.lang.management.ManagementFactory;
+import java.lang.management.MemoryMXBean;
+import java.lang.management.MemoryManagerMXBean;
+import java.lang.management.MemoryPoolMXBean;
+import java.lang.management.MemoryUsage;
+import java.util.List;
+
+import javax.management.MBeanServer;
+
+import org.apache.commons.lang3.ArrayUtils;
+
+public class Heap
+{
+    @SuppressWarnings("unused")
+    public static void main(String[] args)
+    {
+        MBeanServer platformMBeanServer = ManagementFactory.getPlatformMBeanServer();
+
+        List<GarbageCollectorMXBean> garbageCollectorMXBeans = ManagementFactory.getGarbageCollectorMXBeans();
+        for (GarbageCollectorMXBean garbageCollectorMXBean : garbageCollectorMXBeans)
+        {
+            System.out.println(garbageCollectorMXBean.getName());
+            System.out.println(garbageCollectorMXBean.getCollectionCount());
+            System.out.println(garbageCollectorMXBean.getCollectionTime());
+            System.out.println(ArrayUtils.toString(garbageCollectorMXBean.getMemoryPoolNames()));
+        }
+
+        List<MemoryManagerMXBean> memoryManagerMXBeans = ManagementFactory.getMemoryManagerMXBeans();
+        for (MemoryManagerMXBean memoryManagerMXBean : memoryManagerMXBeans)
+        {
+            System.out.println(ArrayUtils.toString(memoryManagerMXBean.getMemoryPoolNames()));
+            System.out.println(memoryManagerMXBean.getName());
+        }
+
+        MemoryMXBean memoryMXBean = ManagementFactory.getMemoryMXBean();
+        MemoryUsage heapMemoryUsage = memoryMXBean.getHeapMemoryUsage();
+        MemoryUsage nonHeapMemoryUsage = memoryMXBean.getNonHeapMemoryUsage();
+
+        List<MemoryPoolMXBean> memoryPoolMXBeans = ManagementFactory.getMemoryPoolMXBeans();
+        for (MemoryPoolMXBean memoryPoolMXBean : memoryPoolMXBeans)
+        {
+            System.out.println(memoryPoolMXBean.getName());
+            System.out.println(memoryPoolMXBean.getType());
+            System.out.println(memoryPoolMXBean.getUsage());
+            System.out.println(memoryPoolMXBean.getCollectionUsage());
+            System.out.println(memoryPoolMXBean.getPeakUsage());
+            System.out.println(ArrayUtils.toString(memoryPoolMXBean.getMemoryManagerNames()));
+        }
+
+        System.out.println("uptime:" + ManagementFactory.getRuntimeMXBean().getUptime());
+    }
+}
