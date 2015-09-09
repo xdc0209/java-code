1ebbb1863ae0a24bee53751ed7c352232ad20d01
xdc0209 <xdc0209@qq.com>
2015-09-09 13:52:17 +0800

    HashMap多线程死循环问题

---
+++ basicKnowledge/src/com/xdc/basic/api/jvm/HashMapInfiniteLoop.java
@@ -0,0 +1,40 @@
+package com.xdc.basic.api.jvm;
+
+import java.util.HashMap;
+
+// 摘自：http://blog.csdn.net/xuefeng0707/article/details/40797085
+//
+// HashMap多线程死循环问题
+//
+// HashMap不是线程安全的，在被多线程共享操作时，有概率出现死循环。这个概率比较低，多次执行代码不一定能复现，但可以通过单步调试构造出来。
+// 问题原因是多线程同时put时，如果同时触发了rehash操作，会导致HashMap中的链表中出现循环节点，进而使得后面get的时候，会死循环。
+// 多线程场景下请使用线程安全的ConcurrentHashMap。
+public class HashMapInfiniteLoop
+{
+    private static HashMap<Integer, Integer> map = new HashMap<Integer, Integer>(2, 0.75f);
+
+    public static void main(String[] args)
+    {
+        map.put(5, 55);
+
+        new Thread("Thread1")
+        {
+            @Override
+            public void run()
+            {
+                map.put(7, 77);
+                System.out.println(map);
+            };
+        }.start();
+
+        new Thread("Thread2")
+        {
+            @Override
+            public void run()
+            {
+                map.put(3, 33);
+                System.out.println(map);
+            };
+        }.start();
+    }
+}
---
+++ basicKnowledge/src/com/xdc/basic/api/jvm/RuntimeInfo.java
@@ -0,0 +1,25 @@
+package com.xdc.basic.api.jvm;
+
+public class RuntimeInfo
+{
+    public static void main(String[] args)
+    {
+        // Total number of processors or cores available to the JVM
+        System.out.println("Total number of processors or cores available to the JVM (cores): "
+                + Runtime.getRuntime().availableProcessors());
+
+        // Total amount of free memory available to the JVM
+        System.out.println("Total amount of free memory available to the JVM (bytes): "
+                + Runtime.getRuntime().freeMemory());
+
+        // This will return Long.MAX_VALUE if there is no preset limit
+        long maxMemory = Runtime.getRuntime().maxMemory();
+        // Maximum amount of memory the JVM will attempt to use
+        System.out.println("Maximum amount of memory the JVM will attempt to use (bytes): "
+                + (maxMemory == Long.MAX_VALUE ? "no limit" : maxMemory));
+
+        // Total memory currently in use by the JVM
+        System.out.println("Total memory currently in use by the JVM - heap size (bytes): "
+                + Runtime.getRuntime().totalMemory());
+    }
+}
