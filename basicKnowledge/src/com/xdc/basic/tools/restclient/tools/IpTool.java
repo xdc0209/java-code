0002b7f861b76529207013c0bc8ba147d55fcc5b
xdc0209 <xdc0209@qq.com>
2016-11-28 22:10:59 +0800

    端口校验

--- basicKnowledge/src/com/xdc/basic/tools/restclient/tools/IpTool.java
+++ basicKnowledge/src/com/xdc/basic/tools/restclient/tools/IpTool.java
@@ -33,6 +33,18 @@ public class IpTool
         return false;
     }
 
+    public static boolean isIpv4PortRegexp(String port)
+    {
+        if (StringUtils.isBlank(port))
+        {
+            return true;
+        }
+
+        String portRegExp = "^[1-9]|[1-9][0-9]|[1-9][0-9]{2}|[1-9][0-9]{3}|[1-5][0-9]{4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5]$";
+
+        return port.matches(portRegExp);
+    }
+
     public static boolean isMask(String mask)
     {
         if (StringUtils.isBlank(mask))
