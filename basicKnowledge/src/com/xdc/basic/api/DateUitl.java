8cd06146502720f3b2ee17e3090222028717ce33
xdc0209 <xdc0209@qq.com>
2016-12-27 21:30:40 +0800

    时间工具类

---
+++ basicKnowledge/src/com/xdc/basic/api/DateUitl.java
@@ -0,0 +1,92 @@
+
+package com.xdc.basic.api;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import org.junit.Assert;
+
+/**
+ * 参考：
+ * http://www.cnblogs.com/springcsc/archive/2009/12/03/1616347.html
+ * http://outofmemory.cn/code-snippet/979/Java-Calendar-Date-usage-summary
+ */
+public class DateUitl
+{
+    /**
+     * 判断是否为月末最后一天
+     */
+    public static boolean isLastDayOfMonth(Date date)
+    {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+
+        return isLastDayOfMonth(calendar);
+    }
+
+    /**
+     * 判断是否为月末最后一天
+     */
+    public static boolean isLastDayOfMonth(Calendar calendar)
+    {
+        int dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH);
+        int actualMaxDayOfMonth = calendar.getActualMaximum(Calendar.DAY_OF_MONTH);
+
+        return dayOfMonth == actualMaxDayOfMonth;
+    }
+
+    /**
+     * 判断是否为月末周六(华为月末周六上班，呵呵)
+     */
+    public static boolean isLastSaturdayOfMonth(Date date)
+    {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+
+        return isLastSaturdayOfMonth(calendar);
+    }
+
+    /**
+     * 判断是否为月末周六(华为月末周六上班，呵呵)
+     */
+    public static boolean isLastSaturdayOfMonth(Calendar calendar)
+    {
+        int dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK);
+        if (dayOfWeek != Calendar.SATURDAY)
+        {
+            return false;
+        }
+
+        int dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH);
+        int actualMaxDayOfMonth = calendar.getActualMaximum(Calendar.DAY_OF_MONTH);
+
+        return dayOfMonth > actualMaxDayOfMonth - 7;
+    }
+
+    public static void main(String[] args)
+    {
+        Assert.assertEquals(false, isLastDayOfMonth(new Date(2016 - 1900, 11 - 1, 29)));
+        Assert.assertEquals(true, isLastDayOfMonth(new Date(2016 - 1900, 11 - 1, 30)));
+        Assert.assertEquals(false, isLastDayOfMonth(new Date(2016 - 1900, 12 - 1, 1)));
+
+        Assert.assertEquals(false, isLastDayOfMonth(new Date(2016 - 1900, 12 - 1, 23)));
+        Assert.assertEquals(false, isLastDayOfMonth(new Date(2016 - 1900, 12 - 1, 24)));
+        Assert.assertEquals(false, isLastDayOfMonth(new Date(2016 - 1900, 12 - 1, 25)));
+
+        Assert.assertEquals(false, isLastDayOfMonth(new Date(2016 - 1900, 12 - 1, 30)));
+        Assert.assertEquals(true, isLastDayOfMonth(new Date(2016 - 1900, 12 - 1, 31)));
+        Assert.assertEquals(false, isLastDayOfMonth(new Date(2017 - 1900, 1 - 1, 1)));
+
+        Assert.assertEquals(false, isLastSaturdayOfMonth(new Date(2016 - 1900, 11 - 1, 25)));
+        Assert.assertEquals(true, isLastSaturdayOfMonth(new Date(2016 - 1900, 11 - 1, 26)));
+        Assert.assertEquals(false, isLastSaturdayOfMonth(new Date(2016 - 1900, 11 - 1, 27)));
+
+        Assert.assertEquals(false, isLastSaturdayOfMonth(new Date(2016 - 1900, 12 - 1, 23)));
+        Assert.assertEquals(false, isLastSaturdayOfMonth(new Date(2016 - 1900, 12 - 1, 24)));
+        Assert.assertEquals(false, isLastSaturdayOfMonth(new Date(2016 - 1900, 12 - 1, 25)));
+
+        Assert.assertEquals(false, isLastSaturdayOfMonth(new Date(2016 - 1900, 12 - 1, 30)));
+        Assert.assertEquals(true, isLastSaturdayOfMonth(new Date(2016 - 1900, 12 - 1, 31)));
+        Assert.assertEquals(false, isLastSaturdayOfMonth(new Date(2017 - 1900, 1 - 1, 1)));
+    }
+}
