d452d79168c124c663c7708fd8320f8caa2d6e35
xdc0209 <xdc0209@qq.com>
2016-12-27 21:32:28 +0800

    元组的使用

--- basicKnowledge/src/com/xdc/basic/skills/tuple/triple/StringTriple.java
+++ basicKnowledge/src/com/xdc/basic/skills/tuple/triple/StringTriple.java
@@ -1,49 +1,57 @@
-package com.xdc.basic.skills.pair;
+package com.xdc.basic.skills.tuple.triple;
 
 import java.io.Serializable;
 
-public class Pair<T1, T2> implements Serializable
+public class StringTriple implements Serializable
 {
-    private static final long serialVersionUID = 2598094911126860655L;
+    private static final long serialVersionUID = 3294424131662177114L;
 
-    private T1                first;
+    private String            first;
 
-    private T2                second;
+    private String            second;
 
-    public Pair(T1 first, T2 second)
+    private String            third;
+
+    public StringTriple(String first, String second, String third)
     {
+        super();
         this.first = first;
         this.second = second;
+        this.third = third;
     }
 
-    public T1 getFirst()
+    public String getFirst()
     {
         return first;
     }
 
-    public void setFirst(T1 first)
+    public void setFirst(String first)
     {
         this.first = first;
     }
 
-    public T2 getSecond()
+    public String getSecond()
     {
         return second;
     }
 
-    public void setSecond(T2 second)
+    public void setSecond(String second)
     {
         this.second = second;
     }
 
-    @Override
-    public String toString()
+    public String getThird()
     {
-        return first + ":" + second;
+        return third;
+    }
+
+    public void setThird(String third)
+    {
+        this.third = third;
     }
 
     /**
-     * 通过eclipse生成
+     * 通过eclipse生成：右键--》源码--》生成hashCode()和equals()
      */
     @Override
     public int hashCode()
@@ -52,51 +60,50 @@ public class Pair<T1, T2> implements Serializable
         int result = 1;
         result = prime * result + ((first == null) ? 0 : first.hashCode());
         result = prime * result + ((second == null) ? 0 : second.hashCode());
+        result = prime * result + ((third == null) ? 0 : third.hashCode());
         return result;
     }
 
     /**
-     * 通过eclipse生成
+     * 通过eclipse生成：右键--》源码--》生成hashCode()和equals()
      */
     @Override
     public boolean equals(Object obj)
     {
         if (this == obj)
-        {
             return true;
-        }
         if (obj == null)
-        {
             return false;
-        }
         if (getClass() != obj.getClass())
-        {
             return false;
-        }
-        Pair<?, ?> other = (Pair<?, ?>) obj;
+        StringTriple other = (StringTriple) obj;
         if (first == null)
         {
             if (other.first != null)
-            {
                 return false;
-            }
         }
         else if (!first.equals(other.first))
-        {
             return false;
-        }
         if (second == null)
         {
             if (other.second != null)
-            {
                 return false;
-            }
         }
         else if (!second.equals(other.second))
-        {
             return false;
+        if (third == null)
+        {
+            if (other.third != null)
+                return false;
         }
+        else if (!third.equals(other.third))
+            return false;
         return true;
     }
 
+    @Override
+    public String toString()
+    {
+        return first + ":" + second + ":" + third;
+    }
 }
--- basicKnowledge/src/com/xdc/basic/skills/tuple/triple/Triple.java
+++ basicKnowledge/src/com/xdc/basic/skills/tuple/triple/Triple.java
@@ -1,48 +1,57 @@
-package com.xdc.basic.skills.pair;
+package com.xdc.basic.skills.tuple.triple;
 
 import java.io.Serializable;
 
-public class StringPair implements Serializable
+public class Triple<T1, T2, T3> implements Serializable
 {
-    private static final long serialVersionUID = 3118267258639752674L;
+    private static final long serialVersionUID = 6073053427205335371L;
 
-    private String            first;
-    private String            second;
+    private T1                first;
 
-    public StringPair(String first, String second)
+    private T2                second;
+
+    private T3                third;
+
+    public Triple(T1 first, T2 second, T3 third)
     {
+        super();
         this.first = first;
         this.second = second;
+        this.third = third;
     }
 
-    public String getFirst()
+    public T1 getFirst()
     {
         return first;
     }
 
-    public void setFirst(String first)
+    public void setFirst(T1 first)
     {
         this.first = first;
     }
 
-    public String getSecond()
+    public T2 getSecond()
     {
         return second;
     }
 
-    public void setSecond(String second)
+    public void setSecond(T2 second)
     {
         this.second = second;
     }
 
-    @Override
-    public String toString()
+    public T3 getThird()
     {
-        return first + ":" + second;
+        return third;
+    }
+
+    public void setThird(T3 third)
+    {
+        this.third = third;
     }
 
     /**
-     * 通过eclipse生成
+     * 通过eclipse生成：右键--》源码--》生成hashCode()和equals()
      */
     @Override
     public int hashCode()
@@ -51,50 +60,50 @@ public class StringPair implements Serializable
         int result = 1;
         result = prime * result + ((first == null) ? 0 : first.hashCode());
         result = prime * result + ((second == null) ? 0 : second.hashCode());
+        result = prime * result + ((third == null) ? 0 : third.hashCode());
         return result;
     }
 
     /**
-     * 通过eclipse生成
+     * 通过eclipse生成：右键--》源码--》生成hashCode()和equals()
      */
     @Override
     public boolean equals(Object obj)
     {
         if (this == obj)
-        {
             return true;
-        }
         if (obj == null)
-        {
             return false;
-        }
         if (getClass() != obj.getClass())
-        {
             return false;
-        }
-        StringPair other = (StringPair) obj;
+        Triple<?, ?, ?> other = (Triple<?, ?, ?>) obj;
         if (first == null)
         {
             if (other.first != null)
-            {
                 return false;
-            }
         }
         else if (!first.equals(other.first))
-        {
             return false;
-        }
         if (second == null)
         {
             if (other.second != null)
-            {
                 return false;
-            }
         }
         else if (!second.equals(other.second))
-        {
             return false;
+        if (third == null)
+        {
+            if (other.third != null)
+                return false;
         }
+        else if (!third.equals(other.third))
+            return false;
         return true;
+    }
+
+    @Override
+    public String toString()
+    {
+        return first + ":" + second + ":" + third;
     }
 }
