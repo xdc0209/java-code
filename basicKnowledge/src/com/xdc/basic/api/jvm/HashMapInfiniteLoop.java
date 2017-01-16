d9888308bab20643be2ebeeb121c1de9fb55cc3f
xdc0209 <xdc0209@qq.com>
2017-01-16 22:20:26 +0800

    HashMap导致死循环

--- basicKnowledge/src/com/xdc/basic/api/jvm/HashMapInfiniteLoop.java
+++ basicKnowledge/src/com/xdc/basic/api/jvm/HashMapInfiniteLoop.java
@@ -6,9 +6,9 @@ import java.util.HashMap;
 //
 // HashMap多线程死循环问题
 //
-// HashMap不是线程安全的，在被多线程共享操作时，有概率出现死循环。这个概率比较低，多次执行代码不一定能复现，但可以通过单步调试构造出来。
+// HashMap不是线程安全的，在被多线程共享操作时，有概率出现死循环，导致单核cpu占用100%。这个概率比较低，多次执行代码不一定能复现，但可以通过单步调试构造出来。
 // 问题原因是多线程同时put时，如果同时触发了rehash操作，会导致HashMap中的链表中出现循环节点，进而使得后面get的时候，会死循环。
-// 多线程场景下请使用线程安全的ConcurrentHashMap。
+// 多线程场景下一定要使用线程安全的ConcurrentHashMap，不能使用HashMap。
 public class HashMapInfiniteLoop
 {
     private static HashMap<Integer, Integer> map = new HashMap<Integer, Integer>(2, 0.75f);
