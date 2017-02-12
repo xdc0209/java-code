c2e12ddea62ef1ca812883be2fc2c884a201d064
xdc0209 <xdc0209@qq.com>
2017-02-12 12:44:43 +0800

    线程池

--- basicKnowledge/src/com/xdc/basic/api/thread/executor/ThreadPoolExecutorTest.java
+++ basicKnowledge/src/com/xdc/basic/api/thread/executor/ThreadPoolExecutorTest.java
@@ -18,11 +18,11 @@ public class ThreadPoolExecutorTest
         ExecutorService pool = Executors.newFixedThreadPool(3);
 
         // 创建实现了Runnable接口对象，Thread对象当然也实现了Runnable接口
-        Thread t1 = new MyThread();
-        Thread t2 = new MyThread();
-        Thread t3 = new MyThread();
-        Thread t4 = new MyThread();
-        Thread t5 = new MyThread();
+        Thread t1 = new MyThread("t1");
+        Thread t2 = new MyThread("t2");
+        Thread t3 = new MyThread("t3");
+        Thread t4 = new MyThread("t4");
+        Thread t5 = new MyThread("t5");
 
         // 将线程放入池中进行执行
         pool.execute(t1);
@@ -35,7 +35,7 @@ public class ThreadPoolExecutorTest
         pool.shutdown();
         System.out.println("Pool state is setted to stop.");
 
-        System.out.println("Wait untill tasks are executed...");
+        System.out.println("Wait until tasks are executed...");
         pool.awaitTermination(60, TimeUnit.MINUTES);
         System.out.println("All tasks are executed...");
     }
@@ -43,13 +43,22 @@ public class ThreadPoolExecutorTest
 
 class MyThread extends Thread
 {
+    String name;
+
+    public MyThread(String name)
+    {
+        super();
+        this.name = name;
+    }
+
     @Override
     public void run()
     {
         try
         {
-            System.out.println(Thread.currentThread().getName() + "正在执行。。。");
+            System.out.println(String.format("[name:%s, threadName:%s] 执行开始.", name, Thread.currentThread().getName()));
             Thread.sleep(1000);
+            System.out.println(String.format("[name:%s, threadName:%s] 执行结束.", name, Thread.currentThread().getName()));
         }
         catch (InterruptedException e)
         {
