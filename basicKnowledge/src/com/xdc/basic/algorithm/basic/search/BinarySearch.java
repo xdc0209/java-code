a3855105ff873c5b047bbbe2ef6c4328a4dc57a5
xdc0209 <xdc0209@qq.com>
2017-10-07 17:52:02 +0800

    算法

--- basicKnowledge/src/com/xdc/basic/algorithm/basic/search/BinarySearch.java
+++ basicKnowledge/src/com/xdc/basic/algorithm/basic/search/BinarySearch.java
@@ -19,6 +19,8 @@ public class BinarySearch
         {
             int mid = (low + high) / 2;
 
+            if (n[mid] > search)
+            {
                 high = mid - 1;
             }
             else if (n[mid] < search)
