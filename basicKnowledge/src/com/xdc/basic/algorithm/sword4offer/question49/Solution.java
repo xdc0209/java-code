2e1a09cce9ce49099683a1df1a16c1a7e4882d8d
xdc0209 <xdc0209@qq.com>
2017-12-07 21:39:43 +0800

    《剑指offer(第2版)》 >>> 面试题49：丑数

--- basicKnowledge/src/com/xdc/basic/algorithm/sword4offer/question49/Solution.java
+++ basicKnowledge/src/com/xdc/basic/algorithm/sword4offer/question49/Solution.java
@@ -129,6 +129,47 @@ public class Solution
         return uglyNumbers[index - 1];
     }
 
+    public static int GetUglyNumber_Solution3(int index)
+    {
+        if (index <= 0)
+        {
+            return 0;
+        }
+
+        int[] uglyNumbers = new int[index];
+        uglyNumbers[0] = 1;
+
+        int t2 = 0;
+        int t3 = 0;
+        int t5 = 0;
+
+        for (int i = 1; i < uglyNumbers.length; i++)
+        {
+            int uglyNumber2 = uglyNumbers[t2] * 2;
+            int uglyNumber3 = uglyNumbers[t3] * 3;
+            int uglyNumber5 = uglyNumbers[t5] * 5;
+
+            uglyNumbers[i] = Math.min(Math.min(uglyNumber2, uglyNumber3), uglyNumber5);
+
+            if (uglyNumbers[i] == uglyNumber2)
+            {
+                t2++;
+            }
+            if (uglyNumbers[i] == uglyNumber3)
+            {
+                t3++;
+            }
+            if (uglyNumbers[i] == uglyNumber5)
+            {
+                t5++;
+            }
+        }
+
+        System.out.println(Arrays.toString(uglyNumbers));
+
+        return uglyNumbers[index - 1];
+    }
+
     public static void main(String[] args)
     {
         System.out.println(GetUglyNumber_Solution(1500));
