fd018fc6d972496db6088cf73049c4b5923f7659
xdc0209 <xdc0209@qq.com>
2017-12-07 12:24:15 +0800

    《剑指offer(第2版)》 >>> 面试题48：最长不包含重复字符的字符串

--- basicKnowledge/src/com/xdc/basic/algorithm/sword4offer/question48/Solution.java
+++ basicKnowledge/src/com/xdc/basic/algorithm/sword4offer/question48/Solution.java
@@ -15,11 +15,13 @@ public class Solution
         int curLen = 0;
 
         int maxLen = 0;
-        int start = -1;
-        int end = -1;
+        int start = -1; // 最长子串的起始索引。
+        int end = -1; // 最长子串的结束索引。
 
         Map<Character, Integer> last = new HashMap<Character, Integer>();
 
+        // i表示正在拼接的子串的起始索引。
+        // j表示正在拼接的子串的结束索引。
         for (int i = 0, j = 0; j < string.length(); j++)
         {
             if (!last.containsKey(string.charAt(j)) || last.get(string.charAt(j)) < i)
